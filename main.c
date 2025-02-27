4d86b1cbfa8878e6a8b19a4022c4c822940948ae
Kiara Guerra <112067327+kguerra3411@users.noreply.github.com>
2025-02-27 04:01:04 -0800

    Update main.c
    resubmit

--- main.c
+++ main.c
@@ -1,78 +1,54 @@
+/* main.c */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-double calculate_ratio(int value, int length) { return (double)value / length; }
-
-int compare_segments(const void *a, const void *b) {
-  int *segmentA = (int *)a;
-  int *segmentB = (int *)b;
-
-  double ratioA = calculate_ratio(segmentA[1], segmentA[0]);
-  double ratioB = calculate_ratio(segmentB[1], segmentB[0]);
-
-  return (ratioA < ratioB) - (ratioA > ratioB);
-}
+#include "cutting_options.h"
+#include "rod_cut.h"
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    printf("rod length invalid\n");
-    return 1;
-  }
-
-  int totalRodLength = atoi(argv[1]);
-  if (totalRodLength <= 0) {
-    printf("Invalid rod length. Please provide an integer greater than 0.\n");
-    return 1;
-  }
-
-  int segmentData[20][2];
-  int numberOfSegments = 0;
-  char userInput[100];
-
-  printf("Enter segment data as <length>, <value>\n");
-
-  while (fgets(userInput, sizeof(userInput), stdin)) {
-    int segmentLength, segmentValue;
-    if (sscanf_s(userInput, "%d, %d", &segmentLength, &segmentValue) == 2) {
-      if (segmentLength > 0 && segmentValue >= 0) {
-        segmentData[numberOfSegments][0] = segmentLength;
-        segmentData[numberOfSegments][1] = segmentValue;
-        numberOfSegments++;
-      } else {
-        printf("Invalid segment data. Ensure length > 0 and value >= 0.\n");
-      }
-    } else {
-      printf("Invalid format. Please use <length>, <value>.\n");
-    }
-  }
-
-  qsort(segmentData, numberOfSegments, sizeof(segmentData[0]),
-        compare_segments);
-
-  int totalValue = 0;
-  int remainingRodLength = totalRodLength;
-
-  for (int i = 0; i < numberOfSegments; i++) {
-    if (remainingRodLength <= 0) {
-      break;
+    if (argc == 1) {
+        fprintf(stderr, "Error: No rod length provided.\n");
+        fprintf(stderr, "Usage: %s <rod_length>\n", argv[0]);
+        return 1;
+    } 
+    else if (argc > 2) {
+        fprintf(stderr, "Error: Too many arguments provided.\n");
+        fprintf(stderr, "Usage: %s <rod_length>\n", argv[0]);
+        return 1;
     }
 
-    int segmentLength = segmentData[i][0];
-    int segmentValue = segmentData[i][1];
-    int maxSegmentsToUse = remainingRodLength / segmentLength;
-
-    if (maxSegmentsToUse > 0) {
-      int valueToAdd = maxSegmentsToUse * segmentValue;
-      printf("\n%d @ %d = %d\n", maxSegmentsToUse, segmentLength, valueToAdd);
-
-      totalValue += valueToAdd;
-      remainingRodLength -= maxSegmentsToUse * segmentLength;
+    char *endptr;
+    long temp = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || temp <= 0) {
+        fprintf(stderr, "Error: Invalid rod length. Must be a positive integer.\n");
+        return 1;
     }
-  }
+    int totalRodLength = (int)temp;
 
-  printf("\nRemaining rod: %d\n", remainingRodLength);
-  printf("Total value: %d\n", totalValue);
+    printf("Enter segment data as <length>, <value>. Press Ctrl+D (or Ctrl+Z on Windows) when done.\n");
+    readcutting_options();
 
-  return 0;
+    if (num_cutting_options == 0) {
+        fprintf(stderr, "Warning: No valid segment data provided. The remaining rod is unchanged.\n");
+        printf("Remaining rod: %d\n", totalRodLength);
+        printf("Total value: 0\n");
+        return 0;
+    }
+
+    int cutCounts[MAX_CUTTING_OPTIONS] = {0};
+    int maxValue = rodCut(totalRodLength, cutCounts);
+
+    printf("Optimal cutting strategy:\n");
+    int remainingRod = totalRodLength;
+    for (int i = 0; i < num_cutting_options; i++) {
+        if (cutCounts[i] > 0) {
+            int totalValue = cutCounts[i] * cutting_options[i].value;
+            printf("%d @ %d = %d\n", cutCounts[i], cutting_options[i].length, totalValue);
+            remainingRod -= cutCounts[i] * cutting_options[i].length;
+        }
+    }
+    
+    printf("Remaining rod: %d\n", remainingRod);
+    printf("Total value: %d\n", maxValue);
+    
+    return 0;
 }
